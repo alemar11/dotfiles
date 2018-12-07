a0c7edeb2a53aed32368c947f51f7d13cc901b2c
Alessandro Marzoli <a.marzoli@ambrogio.com>
2018-12-07 14:27:58 +0100

    new script

--- script.swift
+++ script.swift
@@ -2,7 +2,39 @@
 
 import Foundation
 
-let ignoredFiles = [ ".DS_Store", ".git", ".gitignore", ".rake_tasks~", "extra", ".", "..", "Gemfile", "Rakefile", "README.md", "script.swift", "Gemfile.lock", "script.playground", "script", "install.sh" ]
+let ignoredFiles = [
+										".DS_Store", 
+										".git", 
+										".gitignore", 
+										".rake_tasks~", 
+										"extra", ".", "..", 
+										"Gemfile", 
+										"Rakefile", 
+										"README.md", 
+										"script.swift", 
+										"Gemfile.lock", 
+										"script", 
+										"install.sh"
+									 ]
+
+let red = "\u{001B}[0;31m"
+let green = "\u{001B}[0;32m"
+let yellow = "\u{001B}[0;33m"
+let blue = "\u{001B}[0;34m"
+let magenta = "\u{001B}[0;35m"
+let cyan = "\u{001B}[0;36m"
+let white = "\u{001B}[0;37m"
+
+extension String {
+	func isAlreadySymlinked(to path: String) -> Bool {
+		guard FileManager.default.fileExists(atPath: path) else { return false }
+		let symlinkPath = try? FileManager.default.destinationOfSymbolicLink(atPath: self)
+		if let symlinkPath = symlinkPath, symlinkPath == path {
+			return true
+		}
+		return false
+	}
+}
 
 let homeURL = URL(string: NSHomeDirectory())!
 let sourceURL = URL(string: FileManager.default.currentDirectoryPath)!
@@ -13,22 +45,23 @@ let files = Set(allFiles.map { $0.lastPathComponent }).subtracting(ignoredFiles)
 for file in files {
 	let sourcePath = sourceURL.appendingPathComponent(file).absoluteString
 	let destinationPath = homeURL.appendingPathComponent(file).absoluteString
-
-	let exist = FileManager.default.fileExists(atPath: destinationPath)
-	if !exist {
-		//create symlink
+	
+	if !destinationPath.isAlreadySymlinked(to: sourcePath) {
 		try! FileManager.default.createSymbolicLink(atPath: destinationPath, withDestinationPath: sourcePath)
-		print("symlinked")
+		print("\(green)\(file) has been symlinked.")
+		
 	} else {
-		print("File \(destinationPath) exists. n it (y/n)?")
+		print("\(white)File \(file) exists. Overwrite it (y/n)?")
 		if let response = readLine(), response == "y" {
 			try! FileManager.default.removeItem(atPath: destinationPath)
 			try! FileManager.default.createSymbolicLink(atPath: destinationPath, withDestinationPath: sourcePath)
-			print("symlinked")
+			print("\(green)\(file) has been overwritten.")
+			
 		} else {
-			print("Skipping \(destinationPath).")
+			print("\(cyan)Skipped.")
 		}
 	}
 }
 
-print("Finished")
+print("\n\(white)Completed 🎉")
+
