1f368efe6d27ea9de4785ed58e5ed48f0b427fef
Alessandro Marzoli <alessandromarzoli@gmail.com>
2019-02-23 21:49:13 +0100

    new option file

---
+++ .zsh/options.zsh
@@ -0,0 +1,10 @@
+# Misc options
+setopt AUTO_CD # If you type foo, and it isn't a command, and it is a directory in your cdpath, go there
+setopt NO_BG_NICE # don't nice background tasks
+setopt NO_HUP
+setopt NO_LIST_BEEP
+setopt LOCAL_OPTIONS # allow functions to have local options
+setopt LOCAL_TRAPS # allow functions to have local traps
+setopt COMPLETE_IN_WORD
+setopt IGNORE_EOF
+setopt PROMPT_SUBST # Enable parameter expansion, command substitution, and arithmetic expansion in the prompt
