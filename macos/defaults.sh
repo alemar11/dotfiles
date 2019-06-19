57c75eca78feed5a00a904a25f7be4bcbdd7618b
Alessandro Marzoli <alessandromarzoli@gmail.com>
2019-06-19 17:22:13 +0200

    update

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -6,34 +6,26 @@
 
 echo "Configuring macOS defaults"
 
-###############################################################################
-# HOUSEKEEPING                                                                #
-###############################################################################
+### HouseKeeping
 
 # Close any open System Preferences panes, to prevent them from overriding
 # settings we’re about to change
 osascript -e 'tell application "System Preferences" to quit'
 
-###############################################################################
-# General Preferences                                                         #
-###############################################################################
+### General Preferences
 
 # Show Battery Percentage
 defaults write com.apple.menuextra.battery ShowPercent -bool true
 # Dark UI
 defaults write NSGlobalDomain AppleInterfaceStyle -string "Dark"
 
-###############################################################################
-# Terminal                                                                    #
-###############################################################################
+### Terminal
 
 # Setup the correct theme
 defaults write com.apple.Terminal "Default Window Settings" -string "AM"
 defaults write com.apple.Terminal "Startup Window Settings" -string "AM"
 
-###############################################################################
-# Finder                                                                      #
-###############################################################################
+### Finder
 
 # Finder: show hidden files by default
 defaults write com.apple.finder AppleShowAllFiles -bool true
@@ -47,9 +39,7 @@ chflags nohidden ~/Library
 # Show the /Volumes folder
 sudo chflags nohidden /Volumes
 
-###############################################################################
-# Safari & WebKit                                                             #
-###############################################################################
+### Safari & WebKit
 
 # Show the full URL in the address bar (note: this still hides the scheme)
 defaults write com.apple.Safari ShowFullURLInSmartSearchField -bool true
@@ -60,9 +50,7 @@ defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
 
-###############################################################################
-# Xcode                                                                       #
-###############################################################################
+### Xcode
 
 # Use 2 spaces for indentation
 defaults write com.apple.dt.Xcode DVTTextIndentTabWidth -int 2
@@ -81,10 +69,7 @@ defaults write com.apple.dt.Xcode DVTTextShowLineNumbers -bool true
 defaults write com.apple.dt.Xcode XCFontAndColorCurrentTheme -string AM.xccolortheme
 defaults write com.apple.dt.Xcode XCFontAndColorCurrentDarkTheme -string AM.xccolortheme
 
-
-###############################################################################
-# Dock                                                                       #
-###############################################################################
+### Dock
 
 # Set the icon size of Dock items to 36 pixels
 defaults write com.apple.dock tilesize -int 36
