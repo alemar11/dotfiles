7ce08c19d754d25a28eb4fb59848f1de556e8074
Alessandro Marzoli <alessandromarzoli@gmail.com>
2019-09-16 09:08:27 +0200

    zsh is the default shell on 10.15

--- macos/install.sh
+++ macos/install.sh
@@ -9,23 +9,6 @@ if [ ! -d macos ]; then
   exit 1
 fi
 
-### Zsh
-
-# If this user's login shell is not already "zsh", attempt to switch.
-TEST_CURRENT_SHELL=$(basename "$SHELL")
-if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
-  # If this platform provides a "chsh" command (not Cygwin), do it, man!
-  if hash chsh >/dev/null 2>&1; then
-    echo "🐚 Time to change your default shell to zsh!"
-    chsh -s $(grep /zsh$ /etc/shells | tail -1)
-    # launches zsh
-    env zsh -l
-  else
-    echo "🐚 I can't change your shell automatically because this system does not have chsh."
-    echo "Please manually change your default shell to zsh!"
-  fi
-fi
-
 open "$DOTFILES/macos/AM.terminal"
 
 echo "Configuring macOS defaults."
