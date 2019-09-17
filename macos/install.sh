9ae530cba403b7df860af978711b37e950365bcf
Alessandro Marzoli <alessandromarzoli@gmail.com>
2019-09-17 10:33:00 +0200

    refine install.sh

--- macos/install.sh
+++ macos/install.sh
@@ -11,6 +11,10 @@ fi
 
 open "$DOTFILES/macos/AM.terminal"
 
+echo "Installing software"
+sh "$DOTFILES/macos/brew.sh"
+echo "Done."
+
 echo "Configuring macOS defaults."
 sh "$DOTFILES/macos/defaults.sh"
 echo "Done. Note that some of these changes require a logout/restart to take effect."
