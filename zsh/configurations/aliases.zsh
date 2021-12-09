eff4fc7d293f00cb19163f0b1b99e74fe54206bf
Alessandro Marzoli <alessandromarzoli@gmail.com>
2021-12-09 09:54:28 +0100

    networkquality

--- zsh/configurations/aliases.zsh
+++ zsh/configurations/aliases.zsh
@@ -66,6 +66,7 @@ alias cleanup="find . -name '*.DS_Store' -type f -ls -delete"
 
 # macOS
 alias o='open .'
+alias speedtest='networkquality'
 
 # Python (`type python3` to find the path)
 alias python='/opt/homebrew/bin/python3'
