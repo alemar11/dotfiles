4e6e08886893f0ba99422bee6ef154379ad13855
Alessandro Marzoli <alessandromarzoli@gmail.com>
2025-09-08 17:23:44 +0200

    zsh: simplify vscode helper to macOS-only, add /opt/homebrew path and safer arg handling

--- zsh/vscode.zsh
+++ zsh/vscode.zsh
@@ -1,56 +1,26 @@
-#https://github.com/qianxinfeng/vscode
+#!/usr/bin/env zsh
+# macOS-only VS Code helpers
+# Usage:
+#  - vs <file|dir>  -> open in VS Code
+#  - vs             -> open current dir
+#  - vsf            -> alias for 'vs .'
+#  - svs            -> sudo vs
 
-#Usage
-#If vs command is called without an argument, launch Visual Studio Code
-#If vs is passed a directory, cd to it and open it in Visual Studio Code
-#If vs is passed a file, open it in Visual Studio Code
-#If vsf command is called, it is equivalent to vs ., opening the current folder in Visual Studio Code
-#If svs command is called, it is like sudo vs, opening the file or folder in Visual Studio Code. Useful for editing system protected files.
-
-if [[ $('uname') == 'Linux' ]]; then
-    _vscode_linux_paths=(
+if [[ "$OSTYPE" = darwin* ]]; then
+    _vscode_paths=(
         "/usr/local/bin/code"
-        "$HOME/bin/code"
-        "/opt/vscode/code"
-    )
-    for _vscode_path in $_vscode_linux_paths; do
-        if [[ -a $_vscode_path ]]; then
-            vs_run() { $_vscode_path $@ >/dev/null 2>&1 &| }
-            vs_run_sudo() {sudo $_vscode_path $@ >/dev/null 2>&1}
-            alias svs=vs_run_sudo
-            alias vs=vs_run
-            break
-        fi
-    done
-
-elif  [[ "$OSTYPE" = darwin* ]]; then
-    _vscode_darwin_paths=(
-        "/usr/local/bin/code"
+        "/opt/homebrew/bin/code"
         "/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code"
         "$HOME/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code"
     )
-    for _vscode_path in $_vscode_darwin_paths; do
-        if [[ -a $_vscode_path ]]; then
-            vs_run () { "$_vscode_path" $* }
-            vs_run_sudo () {sudo "$_vscode_path" $* }
+    for _vscode_path in $_vscode_paths; do
+        if [[ -x $_vscode_path ]]; then
+            vs_run () { "$_vscode_path" "$@" }
+            vs_run_sudo () { sudo "$_vscode_path" "$@" }
+            alias vs=vs_run
+            alias vsf='vs .'
             alias svs=vs_run_sudo
-            alias vs=vs_run
             break
         fi
     done
-
-elif [[ "$OSTYPE" = 'cygwin' ]]; then
-    _vscode_cygwin_paths=(
-        "$(cygpath $ProgramW6432/Visual\ Studio\ Code)/code.exe"
-    )
-    for _vscode_path in $_vscode_cygwin_paths; do
-        if [[ -a $_vscode_path ]]; then
-            vs_run () { "$_vscode_path" $* }
-            alias vs=vs_run
-            break
-        fi
-    done
-
 fi
-
-alias vsf='vs .'
